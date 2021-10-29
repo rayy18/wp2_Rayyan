d20fb3187ab9cbb5e1e633adb83cfdb60759cef0
rayyan12200250 <masterayyan@gmail.com>
2021-10-29 12:29:53 +0700

    contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama Saya Rayyan Ghinan Amiruddin
+        Saya tinggal di Probolinggo
+        olahraga yang saya sukai adalah
+        sepak bola";
+    }
+}
